0f46818c60a65b736698dc386c7434cf328236f6
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2021-12-08 00:50:07 +0000

    Add Extensions for Dictionary and Type

---
+++ src/NP.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NP.Common.Extensions
+{
+	/// <summary>
+	/// Extensions for <see cref="IDictionary{TKey, TValue}"/>.
+	/// </summary>
+	public static class DictionaryExtensions
+	{
+		/// <summary>
+		/// Gets a value or generates one with <paramref name="factory"/> and then returns the value..
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="dictionary">This dictionary.</param>
+		/// <param name="key">The key to fetch.</param>
+		/// <param name="factory">Value factory.</param>
+		/// <returns>The <typeparamref name="TValue"/> related to the <paramref name="key"/>.</returns>
+		/// <exception cref="ArgumentNullException">
+		/// When the <paramref name="dictionary"/> or <paramref name="factory"/>
+		/// are null.
+		/// </exception>
+		public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+			Func<TKey, TValue> factory)
+		{
+			if (dictionary == null)
+			{
+				throw new ArgumentNullException(nameof(dictionary));
+			}
+
+			if (factory == null)
+			{
+				throw new ArgumentNullException(nameof(factory));
+			}
+
+			if (dictionary.TryGetValue(key, out var result))
+			{
+				return result;
+			}
+
+			var generatedValue = factory(key);
+			dictionary.Add(key, generatedValue);
+
+			return generatedValue;
+		}
+	}
+}
---
+++ src/NP.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace NP.Common.Extensions
+{
+	/// <summary>
+	/// Extensions for <see cref="Type"/>.
+	/// </summary>
+	public static class TypeExtensions
+	{
+		/// <summary>
+		/// Does <paramref name="thisType"/> implement <typeparamref name="TInterface"/>.
+		/// </summary>
+		/// <typeparam name="TInterface">The interface type that has been inherited.</typeparam>
+		/// <param name="thisType">The type that needs to implement <typeparamref name="TInterface"/>.</param>
+		/// <returns>True when <paramref name="thisType"/> implements <typeparamref name="TInterface"/>.</returns>
+		public static bool HasInterface<TInterface>(this Type @thisType)
+		{
+			return @thisType.HasInterface(typeof(TInterface));
+		}
+
+		/// <summary>
+		/// Does <paramref name="thisType"/> implement <paramref name="interfaceType"/>.
+		/// </summary>
+		/// <param name="thisType">The type that needs to implement <paramref name="interfaceType"/>.</param>
+		/// <param name="interfaceType">The interface type that has been inherited.</param>
+		/// <returns>True when <paramref name="thisType"/> implements <paramref name="interfaceType"/>.</returns>
+		public static bool HasInterface(this Type @thisType, Type interfaceType)
+		{
+
+			return interfaceType.IsAssignableFrom(@thisType) ||
+				   @thisType.GetInterfaces().Any(item => item == interfaceType
+														 || item.IsGenericType
+															 && item.GetGenericTypeDefinition() == interfaceType);
+		}
+	}
+}
