68115e2358ded57f66bf18e0268e40f3bc9d3b92
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2021-12-08 00:50:56 +0000

    Add Maybe and OR classes

---
+++ src/NP.Common/Maybe.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NP.Common
+{
+	/// <summary>
+	///  <see cref="Maybe{T}"/> works as <see cref="Nullable{T}"/> but for both classes and structs.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class Maybe<T> : IEquatable<Maybe<T>>
+	{
+		/// <summary>
+		/// Has a value ?
+		/// </summary>
+		public bool HasValue { get; }
+
+		/// <summary>
+		/// The actual value.
+		/// </summary>
+		public T Value { get; }
+
+		private Maybe()
+		{
+			HasValue = false;
+			Value = default;
+		}
+
+		private Maybe(T value)
+		{
+			HasValue = true;
+			Value = value;
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Maybe{T}"/> with the <paramref name="value"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>The <see cref="Maybe{T}"/> with the <paramref name="value"/>.</returns>
+		public static Maybe<T> WithValue(T value)
+		{
+			return new Maybe<T>(value);
+		}
+
+		/// <summary>
+		/// An Empty <see cref="Maybe{T}"/> value.
+		/// </summary>
+		public static Maybe<T> Empty { get; } = new Maybe<T>();
+
+		/// <inheritdoc />
+		public bool Equals(Maybe<T> other)
+		{
+			if (ReferenceEquals(null, other))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			var isTrue = HasValue == other.HasValue;
+			if(Value is IEquatable<T> equatable)
+			{
+				isTrue = isTrue && equatable.Equals(other.Value);
+			}
+			else
+			{
+				isTrue = isTrue && EqualityComparer<T>.Default.Equals(Value, other.Value);
+			}
+
+			return isTrue;
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, obj))
+			{
+				return true;
+			}
+
+			if (obj.GetType() != GetType())
+			{
+				return false;
+			}
+
+			return Equals((Maybe<T>)obj);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return HasValue.GetHashCode() * 397 ^ EqualityComparer<T>.Default.GetHashCode(Value);
+			}
+		}
+
+		/// <inheritdoc />
+		public static bool operator ==(Maybe<T> left, Maybe<T> right)
+		{
+			return Equals(left, right);
+		}
+
+		/// <inheritdoc />
+		public static bool operator !=(Maybe<T> left, Maybe<T> right)
+		{
+			return !Equals(left, right);
+		}
+	}
+}
---
+++ src/NP.Common/OR.cs
@@ -0,0 +1,153 @@
+﻿namespace NP.Common
+{
+	/// <summary>
+	/// A class providing two optional values.
+	/// </summary>
+	/// <typeparam name="T1"></typeparam>
+	/// <typeparam name="T2"></typeparam>
+	public class OR<T1, T2>
+	{
+		/// <summary>
+		/// The left part of the <see cref="OR{T1, T2}"/>.
+		/// </summary>
+		public Maybe<T1> Left { get; }
+
+		/// <summary>
+		/// The right part of the <see cref="OR{T1, T2}"/>.
+		/// </summary>
+		public Maybe<T2> Right { get; }
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2}"/> with only the <seealso cref="Left" /> part.
+		/// </summary>
+		/// <param name="left"></param>
+		public OR(T1 left)
+		{
+			Left = Maybe<T1>.WithValue(left);
+			Right = Maybe<T2>.Empty;
+		}
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2}"/> with only the <seealso cref="Right" /> part.
+		/// </summary>
+		/// <param name="right"></param>
+		public OR(T2 right)
+		{
+			Left = Maybe<T1>.Empty;
+			Right = Maybe<T2>.WithValue(right);
+		}
+
+		/// <summary>
+		/// Extract the <see cref="Left"/> part of the <seealso cref="OR{T1, T2}"/>.
+		/// </summary>
+		/// <param name="or"></param>
+		public static implicit operator T1(OR<T1, T2> or) => or.Left.Value;
+
+		/// <summary>
+		/// Extract the <see cref="Right"/> part of the <seealso cref="OR{T1, T2}"/>.
+		/// </summary>
+		/// <param name="or"></param>
+		public static implicit operator T2(OR<T1, T2> or) => or.Right.Value;
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2}"/> with only the <seealso cref="Left" /> part.
+		/// </summary>
+		/// <param name="left"></param>
+		public static implicit operator OR<T1, T2>(T1 left) => new OR<T1, T2>(left);
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2}"/> with only the <seealso cref="Right" /> part.
+		/// </summary>
+		/// <param name="right"></param>
+		public static implicit operator OR<T1, T2>(T2 right) => new OR<T1, T2>(right);
+	}
+	/// <summary>
+	/// A class providing three optional values.
+	/// </summary>
+	public class OR<T1, T2, T3>
+	{
+		/// <summary>
+		/// The left part of the <see cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		public Maybe<T1> Left { get; }
+
+		/// <summary>
+		/// The middle part of the <see cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		public Maybe<T2> Middle { get; }
+
+		/// <summary>
+		/// The right part of the <see cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		public Maybe<T3> Right { get; }
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Left" /> part.
+		/// </summary>
+		/// <param name="left"></param>
+		public OR(T1 left)
+		{
+			Left = Maybe<T1>.WithValue(left);
+			Middle = Maybe<T2>.Empty;
+			Right = Maybe<T3>.Empty;
+		}
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Middle" /> part.
+		/// </summary>
+		/// <param name="middle"></param>
+		public OR(T2 middle)
+		{
+			Left = Maybe<T1>.Empty;
+			Middle = Maybe<T2>.WithValue(middle);
+			Right = Maybe<T3>.Empty;
+		}
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Right" /> part.
+		/// </summary>
+		/// <param name="right"></param>
+		public OR(T3 right)
+		{
+			Left = Maybe<T1>.Empty;
+			Middle = Maybe<T2>.Empty;
+			Right = Maybe<T3>.WithValue(right);
+		}
+
+		/// <summary>
+		/// Extract the <see cref="Left"/> part of the <seealso cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		/// <param name="or"></param>
+		public static implicit operator T1(OR<T1, T2, T3> or) => or.Left.Value;
+
+		/// <summary>
+		/// Extract the <see cref="Middle"/> part of the <seealso cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		/// <param name="or"></param>
+		public static implicit operator T2(OR<T1, T2, T3> or) => or.Middle.Value;
+
+		/// <summary>
+		/// Extract the <see cref="Right"/> part of the <seealso cref="OR{T1, T2, T3}"/>.
+		/// </summary>
+		/// <param name="or"></param>
+		public static implicit operator T3(OR<T1, T2, T3> or) => or.Right.Value;
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Left" /> part.
+		/// </summary>
+		/// <param name="left"></param>
+		public static implicit operator OR<T1, T2, T3>(T1 left) => new OR<T1, T2, T3>(left);
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Middle" /> part.
+		/// </summary>
+		/// <param name="middle"></param>
+		public static implicit operator OR<T1, T2, T3>(T2 middle) => new OR<T1, T2, T3>(middle);
+
+		/// <summary>
+		/// Constructs an <see cref="OR{T1, T2, T3}"/> with only the <seealso cref="Right" /> part.
+		/// </summary>
+		/// <param name="right"></param>
+		public static implicit operator OR<T1, T2, T3>(T3 right) => new OR<T1, T2, T3>(right);
+	}
+}
