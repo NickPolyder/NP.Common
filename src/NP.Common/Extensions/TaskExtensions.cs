bd9fe5f88cedbf197d68fa3ca0152f19f8a5df46
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2022-06-08 18:49:04 +0100

    Task helpers

---
+++ src/NP.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+using System.Globalization;
+
+namespace NP.Common.Extensions
+{
+	/// <summary>
+	/// Extensions related to Tasks.
+	/// </summary>
+	public static class TaskExtensions
+	{
+		/// <summary>
+		/// From https://github.com/aspnet/AspNetIdentity/blob/main/src/Microsoft.AspNet.Identity.Core/AsyncHelper.cs
+		/// </summary>
+
+		private static readonly TaskFactory _myTaskFactory = new TaskFactory(CancellationToken.None,
+			TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+		/// <summary>
+		///  Wrap to a Task result.
+		/// </summary>
+		/// <param name="obj">The object to be wrapped.</param>
+		/// <returns></returns>
+		public static Task<TResult> AsTask<TResult>(this TResult obj)
+		{
+			return Task.FromResult(obj);
+		}
+
+		/// <summary>
+		///  Wrap to a Task exception.
+		/// </summary>
+		/// <param name="obj">The object to be wrapped.</param>
+		/// <returns></returns>
+		public static Task AsTask(this Exception obj)
+		{
+			return Task.FromException(obj);
+		}
+
+		/// <summary>
+		///  Wrap to a Task exception.
+		/// </summary>
+		/// <param name="obj">The object to be wrapped.</param>
+		/// <returns></returns>
+		public static Task<TResult> AsTask<TResult>(this Exception obj)
+		{
+			return Task.FromException<TResult>(obj);
+		}
+
+		/// <summary>
+		/// Runs a Task synchronously.
+		/// </summary>
+		/// <typeparam name="TResult"></typeparam>
+		/// <param name="task"></param>
+		/// <returns></returns>
+		public static TResult RunSync<TResult>(this Task<TResult> task)
+		{
+			return RunSync(() => task);
+		}
+
+		/// <summary>
+		/// Runs a Task synchronously.
+		/// </summary>
+		/// <param name="task"></param>
+		public static void RunSync(this Task task)
+		{
+			RunSync(() => task);
+		}
+
+		/// <summary>
+		/// Runs a Task synchronously.
+		/// </summary>
+		/// <typeparam name="TResult"></typeparam>
+		/// <param name="func"></param>
+		/// <returns></returns>
+		public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+		{
+			var cultureUi = CultureInfo.CurrentUICulture;
+			var culture = CultureInfo.CurrentCulture;
+			return _myTaskFactory.StartNew(() =>
+			{
+				Thread.CurrentThread.CurrentCulture = culture;
+				Thread.CurrentThread.CurrentUICulture = cultureUi;
+				return func();
+			}).Unwrap().GetAwaiter().GetResult();
+		}
+
+		/// <summary>
+		/// Runs a Task synchronously. 
+		/// </summary>
+		/// <param name="func"></param>
+		public static void RunSync(Func<Task> func)
+		{
+			var cultureUi = CultureInfo.CurrentUICulture;
+			var culture = CultureInfo.CurrentCulture;
+			_myTaskFactory.StartNew(() =>
+			{
+				Thread.CurrentThread.CurrentCulture = culture;
+				Thread.CurrentThread.CurrentUICulture = cultureUi;
+				return func();
+			}).Unwrap().GetAwaiter().GetResult();
+		}
+	}
+}
---
+++ src/NP.Common/Extensions/TaskHelper.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NP.Common.Extensions
+{
+	/// <summary>
+	/// A helper that allows a delegate to be transformed to a Task
+	/// </summary>
+	public class TaskHelper
+	{
+		/// <summary>
+		/// Turn a processor to a dummy task.
+		/// </summary>
+		/// <param name="processor"></param>
+		/// <param name="request"></param>
+		/// <param name="cancellationToken"></param>
+		/// <typeparam name="TRequest"></typeparam>
+		/// <typeparam name="TResponse"></typeparam>
+		/// <returns></returns>
+		public static Task<TResponse> ConvertToTask<TRequest, TResponse>(Func<TRequest, TResponse> processor,
+			TRequest request,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					return Task.FromCanceled<TResponse>(cancellationToken);
+				}
+
+				return Task.FromResult(processor(request));
+			}
+			catch (System.Exception ex)
+			{
+				return Task.FromException<TResponse>(ex);
+			}
+		}
+
+		/// <summary>
+		/// Turn a processor to a dummy task.
+		/// </summary>
+		/// <param name="processor"></param>
+		/// <param name="cancellationToken"></param>
+		/// <typeparam name="TResponse"></typeparam>
+		/// <returns></returns>
+		public static Task<TResponse> ConvertToTask<TResponse>(Func<TResponse> processor,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					return Task.FromCanceled<TResponse>(cancellationToken);
+				}
+
+				return Task.FromResult(processor());
+			}
+			catch (System.Exception ex)
+			{
+				return Task.FromException<TResponse>(ex);
+			}
+		}
+
+		/// <summary>
+		/// Turn a processor to a dummy task.
+		/// </summary>
+		/// <param name="processor"></param>
+		/// <param name="request"></param>
+		/// <param name="cancellationToken"></param>
+		/// <typeparam name="TRequest"></typeparam>
+		/// <returns></returns>
+		public static Task ConvertToTask<TRequest>(Action<TRequest> processor,
+			TRequest request,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					return Task.FromCanceled(cancellationToken);
+				}
+
+				processor(request);
+				return Task.CompletedTask;
+			}
+			catch (System.Exception ex)
+			{
+				return Task.FromException(ex);
+			}
+		}
+
+		/// <summary>
+		/// Turn a processor to a dummy task.
+		/// </summary>
+		/// <param name="processor"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		public static Task ConvertToTask(Action processor,
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					return Task.FromCanceled(cancellationToken);
+				}
+
+				processor();
+				return Task.CompletedTask;
+			}
+			catch (System.Exception ex)
+			{
+				return Task.FromException(ex);
+			}
+		}
+	}
+}
