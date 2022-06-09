c746b4bba8e9b306fc3ceb69acdf982a2514ba32
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2022-06-10 00:09:25 +0100

    Mapping Functionality and Sample

---
+++ src/NP.Common.AspNetCore/Responses/ResponseToObjectResultMapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using NP.Common.Responses;
+
+namespace NP.Common.AspNetCore.Responses
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ResponseToObjectResultMapper: IMapResponses<IActionResult>
+	{
+		/// <inheritdoc />
+		public IActionResult Map(IResponse response)
+		{
+			switch (response)
+			{
+				case SuccessResponse successResponse:
+					return new OkObjectResult(successResponse.Message);
+				
+			}
+
+			return new ObjectResult(null)
+			{
+				StatusCode = (int)HttpStatusCode.InternalServerError
+			};
+		}
+
+		/// <inheritdoc />
+		public IActionResult Map<TData>(IResponse<TData> response)
+		{
+			switch (response)
+			{
+				case SuccessResponse<TData> successResponse when successResponse.Payload.HasValue:
+					return new OkObjectResult(successResponse.Payload.Value);
+
+			}
+
+			return new ObjectResult(null)
+			{
+				StatusCode = (int)HttpStatusCode.InternalServerError
+			};
+		}
+	}
+}
---
+++ src/NP.Common/Responses/IMapResponses.cs
@@ -0,0 +1,24 @@
+﻿namespace NP.Common.Responses
+{
+	/// <summary>
+	/// Implementors will Map Responses from <see cref="IResponse"/> to <typeparamref name="T"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IMapResponses<out T>
+	{
+		/// <summary>
+		/// Map a <paramref name="response"/> to <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="response"></param>
+		/// <returns></returns>
+		T Map(IResponse response);
+
+		/// <summary>
+		/// Map a <paramref name="response"/> to <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="TData"></typeparam>
+		/// <param name="response"></param>
+		/// <returns></returns>
+		T Map<TData>(IResponse<TData> response);
+	}
+}
